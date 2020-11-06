abbcd793262a60bd1bc7fb0d5949546f0ba3b5ff
willdharris <66787651+willdharris@users.noreply.github.com>
2020-11-06 17:30:43 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 import { getTok } from "./tok.js";
 // import Schedule from "./schedules.js";
 // import { getSchedule } from "schedules";
-let currToken = getTok();
+let currToken = await getTok();
 console.log(`CurrToken = ${currToken}`);
 
-const disc = new Schedule("disc", "56905");
-const ahc = new Schedule("ahc", "18284");
-const apl = new Schedule("apl", "57394");
-const dam = new Schedule("dam", "60468");
-const dfc = new Schedule("dfc", "16618");
-const idd = new Schedule("idd", "16615");
-const mtd = new Schedule("mtd", "31046");
-const own = new Schedule("own", "70388");
-const tlc = new Schedule("tlc", "57391");
-const des = new Schedule("des", "19247");
-const dsf = new Schedule("dsf", "58428");
-const hgtv = new Schedule("hgtv", "49788");
-const cook = new Schedule("cook", "68065");
-const diy = new Schedule("diy", "67375");
-const food = new Schedule("food", "50747");
-const trav = new Schedule("trav", "59303");
+// const disc = new Schedule("disc", "56905");
+// const ahc = new Schedule("ahc", "18284");
+// const apl = new Schedule("apl", "57394");
+// const dam = new Schedule("dam", "60468");
+// const dfc = new Schedule("dfc", "16618");
+// const idd = new Schedule("idd", "16615");
+// const mtd = new Schedule("mtd", "31046");
+// const own = new Schedule("own", "70388");
+// const tlc = new Schedule("tlc", "57391");
+// const des = new Schedule("des", "19247");
+// const dsf = new Schedule("dsf", "58428");
+// const hgtv = new Schedule("hgtv", "49788");
+// const cook = new Schedule("cook", "68065");
+// const diy = new Schedule("diy", "67375");
+// const food = new Schedule("food", "50747");
+// const trav = new Schedule("trav", "59303");
 
 let chanArr = [
   disc,
