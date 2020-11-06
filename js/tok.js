3db60ade7a588b14f5e362fa7bc5c5fc3ff07359
willdharris <66787651+willdharris@users.noreply.github.com>
2020-11-06 16:51:52 -0500

    tok as index js

--- js/tok.js
+++ js/tok.js
@@ -1,5 +1,6 @@
 
 export const getTok = async function () {
+  try {
   const stuff = {
     username: "unclebillepg",
     password: "526D1A95E3B6B19651774D1F90A68C32D758858F",
@@ -14,9 +15,12 @@ export const getTok = async function () {
     getIT
   );
   const jsonTok = await newTok.json();
-  const taken = await jsonTok.token;
+  export const taken = await jsonTok.token;
   // const strToken = JSON.stringify(token);
   console.log(taken);
   return taken;
+} catch {
+  console.log(`We're having trouble connecting to the schedules server. Please try again later.`);
+}
 }
 
