dc8bce8fd14b7a1f77dd1bca8aef4973153c1099
willdharris <66787651+willdharris@users.noreply.github.com>
2020-11-08 16:28:38 -0500

    back to index

--- js/tok2.js
+++ js/tok2.js
@@ -31,30 +31,30 @@ import { user, password } from "./creds.js";
 // }
 // }
 
-// export async function getTok () {
-//   try {
-//   const stuff = {
-//     username: user,
-//     password: password,
-//   };
+export async function getTok () {
+  try {
+  const stuff = {
+    username: user,
+    password: password,
+  };
 
-//   const getIT = {
-//     method: "POST",
-//     body: JSON.stringify(stuff),
-//   };
-//   const newTok = await fetch(
-//     "https://cors-anywhere.herokuapp.com/https://json.schedulesdirect.org/20141201/token",
-//     getIT
-//   );
-//   const jsonTok = await newTok.json();
-//   const taken = await jsonTok.token;
-//   // const strToken = JSON.stringify(token);
-//   console.log(taken);
-//   return taken;
-// } catch {
-//   console.log(`We're having trouble connecting to the schedules server. Please try again later.`);
-// }
-// }
+  const getIT = {
+    method: "POST",
+    body: JSON.stringify(stuff),
+  };
+  const newTok = await fetch(
+    "https://cors-anywhere.herokuapp.com/https://json.schedulesdirect.org/20141201/token",
+    getIT
+  );
+  const jsonTok = await newTok.json();
+  const taken = await jsonTok.token;
+  // const strToken = JSON.stringify(token);
+  console.log(taken);
+  return taken;
+} catch {
+  console.log(`We're having trouble connecting to the schedules server. Please try again later.`);
+}
+}
 
 export default getTok().then(() => {
   const stuff = {
