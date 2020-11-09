4f4a7f1457c7dca018728aa9bf0f48faf1a3f87b
willdharris <66787651+willdharris@users.noreply.github.com>
2020-11-09 11:22:57 -0500

    promise fetch

--- js/tok.js
+++ js/tok.js
@@ -1,37 +1,50 @@
 // import Schedule from "./schedules.js";
 import { user, password } from "./creds.js";
 
-export async function getTok () {
-  try {
-  const stuff = {
-    username: user,
-    password: password,
-  };
+// export async function getTok () {
+//   try {
+//   const stuff = {
+//     username: user,
+//     password: password,
+//   };
 
-  const getIT = {
-    method: "POST",
-    body: JSON.stringify(stuff),
-  };
-  const newTok = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://json.schedulesdirect.org/20141201/token",
-    getIT
-  );
-  const jsonTok = await newTok.json();
-  const taken = await jsonTok.token;
-  // const strToken = JSON.stringify(token);
-  console.log(taken);
-  // return taken;
-} catch {
-  console.log(`We're having trouble connecting to the schedules server. Please try again later.`);
-}
-}
+//   const getIT = {
+//     method: "POST",
+//     body: JSON.stringify(stuff),
+//   };
+//   const newTok = await fetch(
+//     "https://cors-anywhere.herokuapp.com/https://json.schedulesdirect.org/20141201/token",
+//     getIT
+//   );
+//   const jsonTok = await newTok.json();
+//   const taken = await jsonTok.token;
+//   // const strToken = JSON.stringify(token);
+//   console.log(taken);
+//   // return taken;
+// } catch {
+//   console.log(`We're having trouble connecting to the schedules server. Please try again later.`);
+// }
+// }
 
-// test
+
 
 
 // channels.chanArr.forEach(function (e) {
 //      e.getSchedule();
 // });
 
+//PROMISE
+const stuff = {
+  username: user,
+  password: password,
+};
+const getIT = {
+  method: "POST",
+  body: JSON.stringify(stuff),
+};
 
-
+const request = fetch(
+  "https://cors-anywhere.herokuapp.com/https://json.schedulesdirect.org/20141201/token",
+  getIT
+);
+console.log(request);
